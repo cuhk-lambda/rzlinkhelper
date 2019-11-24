23aea494e2df8f30988be2e0e98e079f30ad9fa3
Outvi V <git@outv.im>
2019-11-24 22:47:25 +0800

    Add setup.py stuffs (untested)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='rzlinkhelper',
+      version='1.0',
+      description='Reasonable link helper.',
+      author='Outvi V',
+      author_email='git@outv.im',
+      url='https://github.com/outloudvi/rzlinkhelper',
+      py_modules=['main'],
+      )
