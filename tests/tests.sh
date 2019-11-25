2589bba556544ed8fc4f8245b6e9ad8928452cc8
Outvi V <git@outv.im>
2019-11-25 13:48:21 +0800

    tests: Update CMaker 1.1 path

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,7 +3,7 @@ mkdir dist
 cd dist
 cmake ../repo
 make --trace 2>/dev/null 1>make.log
-wget https://github.com/SchrodingerZhu/cmaker/releases/download/1.0/cmaker -O cmaker
+wget https://github.com/SchrodingerZhu/cmaker/releases/download/1.1/cmaker -O cmaker
 chmod +x cmaker
 ./cmaker -t make.log -o output.log -w $(realpath .)
 python3 ../../main.py output.log
