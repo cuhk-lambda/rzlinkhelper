8ba21fb6feaf7f1fa16e275ba0c66caab90f0100
Outvi V <git@outv.im>
2019-11-29 01:04:05 +0800

    Apply tests at 44e607e

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+
+CMAKER_UPSTREAM="outloudvi/cmaker"
+
 git submodule init
 git submodule update
 mkdir dist
 cd dist
+sed -i 's/VERSION 3.15/VERSION 3.10/' ../repo/CMakeLists.txt  # Since we only have 3.10/3.13
 cmake ../repo
-make --trace 2>/dev/null 1>make.log
-wget https://github.com/SchrodingerZhu/cmaker/releases/download/1.1/cmaker -O cmaker
+remake -x 2>/dev/null >make.log
+curl https://api.github.com/repos/$CMAKER_UPSTREAM/releases/latest | jq .assets_url | xargs curl | jq .[0].browser_download_url | xargs -IX -- curl X -L -O cmaker
 chmod +x cmaker
 ./cmaker -t make.log -o output.log -w $(realpath .)
+cat > settings.json <<EOF
+{
+    "callpass_library_path": "./libcallpass.so",
+    "object_dir": "objects",
+    "target_dir": "targets",
+    "debug": true,
+    "original_cxx_executable": "/usr/bin/c++",
+    "original_cc_executable": "/usr/bin/cc",
+    "targeted_cxx_executable": "/usr/bin/clang++-7",
+    "targeted_cc_executable": "/usr/bin/clang",
+    "llvm_link_executable": "/usr/bin/llvm-link-7"
+}
+EOF
 python3 ../../main.py output.log
