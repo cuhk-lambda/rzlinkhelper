3e344c7e89fe007889358dcfc7087e79ba69a5de
Outvi V <git@outv.im>
2019-11-29 02:33:58 +0800

    tests: Try to fetch the latest version of cmaker

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+CMAKER_UPSTREAM_API="outloudvi/cmaker"
+
 git submodule init
 git submodule update
 mkdir dist
 cd dist
-sed -i 's/VERSION 3.15/VERSION 3.10/' ../repo/CMakeLists.txt 
+sed -i 's/VERSION 3.15/VERSION 3.10/' ../repo/CMakeLists.txt  # Since we only have 3.10/3.13
 cmake ../repo
 remake -x 2>/dev/null >make.log
-wget https://github.com/SchrodingerZhu/cmaker/releases/download/1.1/cmaker -O cmaker
+curl https://api.github.com/repos/$CMAKER_UPSTREAM/releases/latest | jq .assets_url | xargs curl | jq .[0].browser_download_url | xargs -IX -- curl X -L -O cmaker
 chmod +x cmaker
 ./cmaker -t make.log -o output.log -w $(realpath .)
 cat > settings.json <<EOF
