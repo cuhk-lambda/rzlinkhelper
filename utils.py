f126d4d3ca046a0ec92c496ffa172708fd3a24c2
schrodingerzhu <i@zhuyi.fan>
2019-11-28 23:22:48 +0800

    reformat and use context manager

--- utils.py
+++ utils.py
@@ -18,112 +18,107 @@ settings = {
     "llvm_link_executable": "/usr/bin/llvm-link"
 }
 
-
 iolock = Lock()
 
 try:
-  custSettings = json.load(open("settings.json"))
-  for i in custSettings.items():
-    settings[i[0]] = i[1]
+    custSettings = json.load(open("settings.json"))
+    for i in custSettings.items():
+        settings[i[0]] = i[1]
 except:
-  pass
+    pass
 
 
 def GET(name):
-  if name in settings:
-    return settings[name]
-  return None
+    if name in settings:
+        return settings[name]
+    return None
 
 
 class Console():
-  @staticmethod
-  def info(*st):
-    iolock.acquire()
-    print(colored("[INFO]", "blue"), *st)
-    iolock.release()
+    @staticmethod
+    def info(*st):
+        with iolock:
+            print(colored("[INFO]", "blue"), *st)
 
-  @staticmethod
-  def warn(*st):
-    iolock.acquire()
-    print(colored("[WARN]", "yellow"), *st)
-    iolock.release()
+    @staticmethod
+    def warn(*st):
+        with iolock:
+          print(colored("[WARN]", "yellow"), *st)
 
-  @staticmethod
-  def error(*st):
-    iolock.acquire()
-    print(colored("[ERRR]", "red"), *st)
-    iolock.release()
+    @staticmethod
+    def error(*st):
+        with iolock:
+          print(colored("[ERRR]", "red"), *st)
 
-  @staticmethod
-  def log(*st):
-    Console.info(*st)
+    @staticmethod
+    def log(*st):
+        Console.info(*st)
 
-  @staticmethod
-  def debug(*st):
-    if GET("debug"):
-      iolock.acquire()
-      print("[DEBG]", *st)
-      iolock.release()
+    @staticmethod
+    def debug(*st):
+        if GET("debug"):
+            with iolock:
+              print("[DEBG]", *st)
 
 
 def checkDir(subdir, name):
-  if not os.access(subdir, os.W_OK):
-    Console.warn(name + " directory unreadable. Trying to create.")
-    try:
-      os.mkdir(subdir)
-      if not os.access(subdir, os.W_OK):
-        raise IOError
-    except:
-      Console.error(name + " directory unwritable")
-      sys.exit(1)
-  if len(os.listdir(subdir)) != 0:
-    Console.warn(name + " directory is not empty!")
+    if not os.access(subdir, os.W_OK):
+        Console.warn(name + " directory unreadable. Trying to create.")
+        try:
+            os.mkdir(subdir)
+            if not os.access(subdir, os.W_OK):
+                raise IOError
+        except:
+            Console.error(name + " directory unwritable")
+            sys.exit(1)
+    if len(os.listdir(subdir)) != 0:
+        Console.warn(name + " directory is not empty!")
 
 
 def hasNoIndirectDependcies(deps):
-  for i in deps:
-    if len(i) > 2 and i[-2:] == ".o":
-      continue
-    else:
-      return False
-  return True
+    for i in deps:
+        if len(i) > 2 and i[-2:] == ".o":
+            continue
+        else:
+            return False
+    return True
 
 
 def findName(path):
-  return path.split("/")[-1]
+    return path.split("/")[-1]
 
 
 def findNames(path):
-  return list(map(findName, path))
+    return list(map(findName, path))
 
 
 def pathToValidNames(path, table):
-  names = []
-  for i in path:
-    if i[-2:] == ".a" or \
-       i[-2:] == ".o" or \
-       i[-3:] == ".so":
-      hashName = sha1sum(i)
-      table[hashName] = i
-      names.append(hashName)
-  return names
+    names = []
+    for i in path:
+        if i[-2:] == ".a" or \
+                i[-2:] == ".o" or \
+                i[-3:] == ".so":
+            hashName = sha1sum(i)
+            table[hashName] = i
+            names.append(hashName)
+    return names
 
 
 def getllvmLinkCmd(destPath, deps):
-  return GET("llvm_link_executable") + " " + " ".join(deps) + " -S -o " + destPath
+    return GET("llvm_link_executable") + " " + " ".join(deps) + " -S -o " + destPath
 
 
 def sha1sum(text):
-  return sha1(text.encode()).hexdigest()
+    return sha1(text.encode()).hexdigest()
 
 
 def deduplicate(items):
-  ret = []
-  for i in items:
-    if i not in ret:
-      ret.append(i)
-  return ret
+    ret = []
+    for i in items:
+        if i not in ret:
+            ret.append(i)
+    return ret
 
 
 def hasNoDependency(fullpath):
-  return fullpath[-2:] == ".o"
+    return fullpath[-2:] == ".o"
--- works.py
+++ works.py
@@ -20,152 +20,150 @@ linkingTaskQueue = Queue()
 
 
 def single_compile(cmd, hashname, execname, r, totalLength):
-  if (os.access(utils.GET("object_dir") + "/" + hashname, os.R_OK)):
-    console.info(
-        "Found {}, skipping. [{}/{}]".format(execname, r+1, totalLength))
+    if (os.access(utils.GET("object_dir") + "/" + hashname, os.R_OK)):
+        console.info(
+            "Found {}, skipping. [{}/{}]".format(execname, r + 1, totalLength))
+        return True
+    else:
+        console.info("Compiling {} [{}/{}]".format(execname, r + 1, totalLength))
+        console.debug(cmd)
+    try:
+        subprocess.run(cmd, shell=True,
+                       stdout=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError:
+        console.error("Error compiling {}".format(execname))
+        sys.exit(2)
     return True
-  else:
-    console.info("Compiling {} [{}/{}]".format(execname, r+1, totalLength))
-    console.debug(cmd)
-  try:
-    subprocess.run(cmd, shell=True,
-                   stdout=subprocess.PIPE, check=True)
-  except subprocess.CalledProcessError:
-    console.error("Error compiling {}".format(execname))
-    sys.exit(2)
-    return False
-  return True
 
 
 def single_linking(top):
-  print(top)
-  if utils.hasNoDependency(sha1Table[top]):
-    console.debug(
-        "{} ({}) has no dependency. Skipping.".format(sha1Table[top], top))
-  else:
-    console.debug(
-        "{} ({}) has some dependencies.".format(sha1Table[top], top))
-    deps = dependencyList[top]["dependencies"]
-    cmdline = utils.getllvmLinkCmd(
-        realpath(utils.GET("object_dir") + "/" + top), list(map(lambda x: utils.GET("object_dir") + "/" + x, deps)))
-    console.debug(cmdline)
-    console.info("Linking " + top)
-    if (os.access(utils.GET("object_dir") + "/" + top, os.R_OK)):
-      console.info("{} found. Skipping".format(top))
+    print(top)
+    if utils.hasNoDependency(sha1Table[top]):
+        console.debug(
+            "{} ({}) has no dependency. Skipping.".format(sha1Table[top], top))
     else:
-      try:
-        subprocess.run(cmdline, shell=True,
-                       stdout=subprocess.PIPE, check=True)
-      except subprocess.CalledProcessError:
-        console.error("Error linking {}".format(top))
-        console.error("Related dependcies:")
-        for i in deps:
-          console.error("{} ({})".format(sha1Table[i], i))
-          console.error("cmdline by original filenames: ",
-                        utils.getllvmLinkCmd(realpath(utils.GET("object_dir") + "/" + sha1Table[top]),
-                                             list(map(lambda x: sha1Table[x], deps))))
-        sys.exit(2)
-        return False
-  finalDepList.append(top)
-  for i in dependentList[top]:
-    lock_countEdit[i].acquire()
-    dependencyList[i]["pendingDepCount"] -= 1
-    print(i, "depcnt: ", dependencyList[i]["pendingDepCount"], list(
-        map(lambda x: sha1Table[x], dependencyList[i]["dependencies"])))
-    lock_countEdit[i].release()
-    if dependencyList[i]["pendingDepCount"] <= 1:
-      print("Pushing", i)
-      linkingTaskQueue.put(i)
-  return True
+        console.debug(
+            "{} ({}) has some dependencies.".format(sha1Table[top], top))
+        deps = dependencyList[top]["dependencies"]
+        cmdline = utils.getllvmLinkCmd(
+            realpath(utils.GET("object_dir") + "/" + top), list(map(lambda x: utils.GET("object_dir") + "/" + x, deps)))
+        console.debug(cmdline)
+        console.info("Linking " + top)
+        if (os.access(utils.GET("object_dir") + "/" + top, os.R_OK)):
+            console.info("{} found. Skipping".format(top))
+        else:
+            try:
+                subprocess.run(cmdline, shell=True,
+                               stdout=subprocess.PIPE, check=True)
+            except subprocess.CalledProcessError:
+                console.error("Error linking {}".format(top))
+                console.error("Related dependcies:")
+                for i in deps:
+                    console.error("{} ({})".format(sha1Table[i], i))
+                    console.error("cmdline by original filenames: ",
+                                  utils.getllvmLinkCmd(realpath(utils.GET("object_dir") + "/" + sha1Table[top]),
+                                                       list(map(lambda x: sha1Table[x], deps))))
+                sys.exit(2)
+    finalDepList.append(top)
+    for i in dependentList[top]:
+        lock_countEdit[i].acquire()
+        dependencyList[i]["pendingDepCount"] -= 1
+        print(i, "depcnt: ", dependencyList[i]["pendingDepCount"], list(
+            map(lambda x: sha1Table[x], dependencyList[i]["dependencies"])))
+        lock_countEdit[i].release()
+        if dependencyList[i]["pendingDepCount"] <= 1:
+            print("Pushing", i)
+            linkingTaskQueue.put(i)
+    return True
 
 
 def do_process(data):
-  # Preparing directories
-  utils.checkDir(utils.GET("object_dir"), "Object")
-  utils.checkDir(utils.GET("target_dir"), "Target")
-  originalCXX = utils.GET("original_cxx_executable")
-  originalCC = utils.GET("original_cc_executable")
+    # Preparing directories
+    utils.checkDir(utils.GET("object_dir"), "Object")
+    utils.checkDir(utils.GET("target_dir"), "Target")
+    originalCXX = utils.GET("original_cxx_executable")
+    originalCC = utils.GET("original_cc_executable")
 
-  totalLength = len(data["compile"])
-  compileTaskPool = Pool()
-  console.log("Compiling .o (total: {})".format(totalLength))
-  for r in range(totalLength):
-    i = data["compile"][r]
-    execname = "(unknown)"
-    cmdline = list(filter(lambda x: x != "", i.split(" ")))
-    for argnum in range(len(cmdline)):
-      if cmdline[argnum] == originalCXX:
-        cmdline[argnum] = utils.GET("targeted_cxx_executable")
-        cmdline[argnum] += " -emit-llvm"
-      elif cmdline[argnum] == originalCC:
-        cmdline[argnum] = utils.GET("targeted_cc_executable")
-        cmdline[argnum] += " -emit-llvm"
-      elif cmdline[argnum] == "-o":
-        filepath = realpath(cmdline[argnum+1])
-        filehashpath = utils.sha1sum(filepath)
-        sha1Table[filehashpath] = filepath
-        cmdline[argnum +
-                1] = realpath(utils.GET("object_dir") + "/" + filehashpath)
-        execname = utils.findName(filepath)
-      elif cmdline[argnum] == "-c":
-        cmdline[argnum] = "-S"
-      elif cmdline[argnum] == "-g":
-        cmdline[argnum] = ""
-    command = " ".join(cmdline)
-    compileTaskPool.apply_async(
-        single_compile, args=(command, filehashpath, execname, r, totalLength), error_callback=console.error)
-    finalDepList.append(filehashpath)
-  compileTaskPool.close()
-  compileTaskPool.join()
+    totalLength = len(data["compile"])
+    compileTaskPool = Pool()
+    console.log("Compiling .o (total: {})".format(totalLength))
+    for r in range(totalLength):
+        i = data["compile"][r]
+        execname = "(unknown)"
+        cmdline = list(filter(lambda x: x != "", i.split(" ")))
+        for argnum in range(len(cmdline)):
+            if cmdline[argnum] == originalCXX:
+                cmdline[argnum] = utils.GET("targeted_cxx_executable")
+                cmdline[argnum] += " -emit-llvm"
+            elif cmdline[argnum] == originalCC:
+                cmdline[argnum] = utils.GET("targeted_cc_executable")
+                cmdline[argnum] += " -emit-llvm"
+            elif cmdline[argnum] == "-o":
+                filepath = realpath(cmdline[argnum + 1])
+                filehashpath = utils.sha1sum(filepath)
+                sha1Table[filehashpath] = filepath
+                cmdline[argnum +
+                        1] = realpath(utils.GET("object_dir") + "/" + filehashpath)
+                execname = utils.findName(filepath)
+            elif cmdline[argnum] == "-c":
+                cmdline[argnum] = "-S"
+            elif cmdline[argnum] == "-g":
+                cmdline[argnum] = ""
+        command = " ".join(cmdline)
+        compileTaskPool.apply_async(
+            single_compile, args=(command, filehashpath, execname, r, totalLength), error_callback=console.error)
+        finalDepList.append(filehashpath)
+    compileTaskPool.close()
+    compileTaskPool.join()
 
-  # Construct the graph
-  console.info("Linking files")
-  graphData = data["scripts"]
+    # Construct the graph
+    console.info("Linking files")
+    graphData = data["scripts"]
 
-  asPath = set()
-  for i in graphData:
-    asPath.add(utils.sha1sum(i["target"]["abs_path"]))
+    asPath = set()
+    for i in graphData:
+        asPath.add(utils.sha1sum(i["target"]["abs_path"]))
 
-  for i in graphData:
-    # Initial data
-    itemPath = i["target"]["abs_path"]
-    hashedItemPath = utils.sha1sum(itemPath)
-    sha1Table[hashedItemPath] = itemPath
-    itemDependencies = i["target"]["dependencies"]
-    itemType = i["target"]["target_type"]
-    dependencyList[hashedItemPath] = {
-        "path": itemPath,
-        "hashed": hashedItemPath,
-        "dependencies": utils.deduplicate(utils.pathToValidNames(itemDependencies, sha1Table)),
-        "type": itemType
-    }
-    dependencyList[hashedItemPath]["pendingDepCount"] = 0
-    for j in dependencyList[hashedItemPath]["dependencies"]:
-      if j in asPath:
-        dependencyList[hashedItemPath]["pendingDepCount"] += 1
-      dependentList[j].append(hashedItemPath)
+    for i in graphData:
+        # Initial data
+        itemPath = i["target"]["abs_path"]
+        hashedItemPath = utils.sha1sum(itemPath)
+        sha1Table[hashedItemPath] = itemPath
+        itemDependencies = i["target"]["dependencies"]
+        itemType = i["target"]["target_type"]
+        dependencyList[hashedItemPath] = {
+            "path": itemPath,
+            "hashed": hashedItemPath,
+            "dependencies": utils.deduplicate(utils.pathToValidNames(itemDependencies, sha1Table)),
+            "type": itemType
+        }
+        dependencyList[hashedItemPath]["pendingDepCount"] = 0
+        for j in dependencyList[hashedItemPath]["dependencies"]:
+            if j in asPath:
+                dependencyList[hashedItemPath]["pendingDepCount"] += 1
+            dependentList[j].append(hashedItemPath)
 
-  for i in finalDepList:
-    for j in dependentList[i]:
-      print(j, sha1Table[j])
-      # dependencyList[j]["pendingDepCount"] -= 1
-      print(j, "depcnt: ", dependencyList[j]["pendingDepCount"], list(
-          map(lambda x: sha1Table[x], dependencyList[j]["dependencies"])))
-      if dependencyList[j]["pendingDepCount"] <= 1:
-        print("Pushing", j)
-        linkingTaskQueue.put(j)
+    for i in finalDepList:
+        for j in dependentList[i]:
+            print(j, sha1Table[j])
+            # dependencyList[j]["pendingDepCount"] -= 1
+            print(j, "depcnt: ", dependencyList[j]["pendingDepCount"], list(
+                map(lambda x: sha1Table[x], dependencyList[j]["dependencies"])))
+            if dependencyList[j]["pendingDepCount"] <= 1:
+                print("Pushing", j)
+                linkingTaskQueue.put(j)
 
-  # print(dependencyList)
-  # print(dependentList)
+    # print(dependencyList)
+    # print(dependentList)
 
-  ctr = len(graphData)
-  while ctr > 0:
-    p = Pool(None)
-    while not linkingTaskQueue.empty():
-      top = linkingTaskQueue.get()
-      ctr -= 1
-      p.apply_async(single_linking, args=(top,), error_callback=console.error)
-    p.close()
-    p.join()
+    ctr = len(graphData)
+    while ctr > 0:
+        p = Pool(None)
+        while not linkingTaskQueue.empty():
+            top = linkingTaskQueue.get()
+            ctr -= 1
+            p.apply_async(single_linking, args=(top,), error_callback=console.error)
+        p.close()
+        p.join()
 
-  console.info("Finished.")
+    console.info("Finished.")
